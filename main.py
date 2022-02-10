18e65213e23a98a2fbf3b3fef1a810153af7f9a1
MuspiMerol <wjj123345567@iCloud.com>
2022-02-10 17:04:08 +0800

    用时分布不再用散点图表示，而是用柱状图

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for i in table["提交答卷时间"]:
 
 def show_submit_time():
     return save_and_show(
-        Grid(init_opts=get_init_options())
+        Grid(get_init_options())
         .add(
             Scatter(init_opts=get_init_options())
             .add_xaxis(index)
@@ -53,35 +53,19 @@ def show_submit_time():
 time_cost = [int(i[:-1]) for i in table["所用时间"]]
 
 def show_time_used():
+    y = [0] * (4 * 6 + 1)
+    for i in time_cost:
+        y[min(i // 15, 24)] += 1
     return save_and_show(
-        Scatter(init_opts=get_init_options())
-        .add_xaxis(index)
-        .add_yaxis("", item, symbol_size=4)
+        Bar(get_init_options())
+        .add_xaxis([f"{i / 4}" for i in range(24)] + ["6+"])
+        .add_yaxis("", y)
         .set_global_opts(
-            title_opts=opts.TitleOpts(subtitle="散点图", title="做题用时的分布"),
-            yaxis_opts=opts.AxisOpts(name="用时/s"),
-            xaxis_opts=opts.AxisOpts(name="答卷序号"),
-        )
-        .set_series_opts(
-            label_opts=opts.LabelOpts(False),
+            title_opts=opts.TitleOpts(subtitle="柱状图", title="填写答卷用时分布"),
+            xaxis_opts=opts.AxisOpts(is_scale=True, name="用时/min"),
+            yaxis_opts=opts.AxisOpts(is_scale=True, name="人数/人"),
         ),
-        f"用时分布_散点图"
-    )
-
-def show_used_comp_submit(table):
-    return save_and_show(
-        Scatter(init_opts=get_init_options())
-        .add_xaxis([int(i[:-1]) for i in table["所用时间"]])
-        .add_yaxis("", [parse_time(i).int_timestamp for i in table["提交答卷时间"]], symbol_size=4)
-        .set_global_opts(
-            title_opts=opts.TitleOpts(subtitle="散点图", title="用时与提交时间的关系"),
-            xaxis_opts=opts.AxisOpts(is_scale=True, name="用时/s"),
-            yaxis_opts=opts.AxisOpts(is_scale=True, name="提交时间/ms"),
-        )
-        .set_series_opts(
-            label_opts=opts.LabelOpts(False),
-        ),
-        f"用时分布与提交时间的关系_散点图"
+        f"用时分布_条形图"
     )
 
 def show_used_comp_submit_2():
